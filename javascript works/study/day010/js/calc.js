50d512e061e27825ee57fe3d3dd316bce072f979
k26k <klee26a@gmail.com>
2022-05-20 18:20:43 +0900

    up

---
+++ javascript works/study/day010/js/calc.js
@@ -0,0 +1,26 @@
+// 덧셈 프로그램
+
+/*
+let n1 = 10;
+let n2 = 20;
+let sumV = 0;
+
+// 연산
+sumV = n1 + n2;
+
+// 출력
+document.write(sumV);
+*/
+
+let n1 = parseInt(prompt("숫자"));
+let n2 = parseInt(prompt("숫자"));
+//parseInt(값) 값을 정수로 전환해줌; 
+let sumV = 0;
+
+// 연산
+sumV = n1 + n2;
+
+// 출력
+document.write("두 수의 합 = " + sumV);
+
+
