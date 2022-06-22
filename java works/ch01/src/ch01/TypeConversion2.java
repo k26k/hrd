51fd387b9425e78d56f23d07e528591264a8a7c6
k26k <klee26a@gmail.com>
2022-06-22 10:17:19 +0900

    .

---
+++ java works/ch01/src/ch01/TypeConversion2.java
@@ -0,0 +1,17 @@
+package ch01;
+
+public class TypeConversion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		사칙연산
+		int x = 10, y = 20;
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y); // int자료형은 계산도 int형으로 해서 소수점 아래 자리가 전부 버려짐
+		System.out.println((double)x/(double)y); // 실수형 double로 컨버전 하고 계산하면 소수점 표시
+	}
+
+}
